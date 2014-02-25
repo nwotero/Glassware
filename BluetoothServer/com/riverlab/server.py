39e7e0f29245139adc0cc17a970d8096b04c14d1
Nick Otero <notero87@gmail.com>
2014-02-25 10:16:03 -0500

    Added BluetoothServer

---
+++ BluetoothServer/com/riverlab/server.py
@@ -0,0 +1,36 @@
+from bluetooth import *
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "1aefbf9b-ea60-47de-b5a0-ed0e3a36d9a5"
+testUuid = "00001101-0000-1000-8000-00805f9b34fb"
+
+advertise_service( server_sock, "GlassServer",
+                   service_id = testUuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
+                    )
+                   
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print("received [%s]" % data)
+except IOError:
+    pass
+
+print("disconnected")
+
+client_sock.close()
+server_sock.close()
+print("all done")
